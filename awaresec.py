8cbb3f8ab75ccbb13c31173ebb1c7796df4907c8
@mey17 <mariamilenavardanyan@gmail.com>
2024-12-10 22:28:58 +0400

    Daily update 5

--- awaresec.py
+++ awaresec.py
@@ -1,11 +1,11 @@
-def print_large_title(title):
+import shutil
 
+def print_large_title(title):
     bold_start = "\033[1m"
     bold_end = "\033[0m"
     green_color = "\033[32m"  # Green color code
     
     print(f"{bold_start}{green_color}{title}{bold_end}")
-
 
 def printTitle():
     title_text = """
@@ -17,7 +17,16 @@ def printTitle():
                  /_/    \_\_/\_/ \__,_|_|  \___|_____/ \___|\___|                
                                                                                  
                                                                                 """
-    print_large_title(title_text)
-    print("https://awaresec.am                  v.1.2                  © Copyright 2024 awaresec.am or one of its authors.")
+    # Get the terminal size
+    terminal_size = shutil.get_terminal_size((80, 20))
+    terminal_width = terminal_size.columns
 
-printTitle()    
+    # Adjust the title text to fit the terminal width
+    adjusted_title_text = "\n".join([line.center(terminal_width) for line in title_text.split("\n")])
+    
+    print_large_title(adjusted_title_text)
+    print("https://awaresec.am".center(terminal_width))
+    print("v.1.2".center(terminal_width))
+    print("© Copyright 2024 awaresec.am or one of its authors.".center(terminal_width))
+
+printTitle()
